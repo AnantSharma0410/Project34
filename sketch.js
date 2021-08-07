b681edb5b6dd35c1581d2ffc402dfee0aad510f5
AnantSharma0410 <72499139+AnantSharma0410@users.noreply.github.com>
2021-08-07 21:11:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
   database=firebase.database();
 	createCanvas(500, 500);
 
-  dog=createSprite(1000,300,150,150);
+  dog=createSprite(250,300,150,150);
   dog.addImage(dogImg);
   dog.scale=0.15;
   
